34287bd8c2b821fac9aef58d5f14a297c83b2960
JoJoWitness <jordano470@gmail.com>
2025-06-28 23:10:21 -0400

    Did the unix one, now, suffering with the windows one

--- Practica5/pipes.c
+++ Practica5/pipes.c
@@ -1,68 +1,16 @@
-// C program to demonstrate use of fork() and pipe()
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <semaphore.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <fcntl.h>
+#include <ctype.h>
+#ifdef __linux__
+    #include "unix.h"
+#elif _WIN32
+    #include "windows.h"
+#endif
+
 
 int main()
-{
-    const int SIZE = 4096;
-    const char* sm_name = "shared_memory_string";
-    char* ptr_sm;
-    int shm_fd = shm_open(sm_name, O_CREAT | O_RDWR, 0666);
-    ftruncate(shm_fd, SIZE);
-
-    sem_t semaphore;
-    sem_init(&semaphore, 0, 1);
-    ptr_sm = mmap(0, SIZE,  PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-
-    // const char* message_0 = "Hello";
-    // const char* message_1 = "World!";
-
-    // char* write_ptr = ptr_sm;
-    // sprintf(write_ptr, "%s", message_0);
-    // write_ptr += strlen(message_0);
-    // sprintf(write_ptr, "%s", message_1);
-    // write_ptr += strlen(message_1);
-
-    // printf("%s\n", ptr_sm);
-    
-    pid_t p1;
-    pid_t p2;
-    int fd[2];
-    if (pipe(fd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-   p1 = fork();
-   p2 = fork();
-
-   if(p1 == 0 && p2 > 0) {
-      char input_str[100];
-      printf("Enter any character string");
-      scanf("%s", input_str);
-
-      close(fd[0]);
-      write(fd[1], input_str, strlen(input_str) + 1);
-      close(fd[1]);
-      exit(0);
-    } else if (p2 == 0 && p1 > 0) {
-        char concat_str[100];
-        close(fd[1]);
-        read(fd[0], concat_str, sizeof(concat_str));
-        printf("Received string: %s\n", concat_str);
-        close(fd[0]); 
-        exit(0);
-    } else if (p1 > 0 && p2 > 0) {
-        // Parent process
-        wait(NULL); // Wait for child processes to finish
-    }
- 
+{   
+    pipeFunction();
     return 0;
 }
---
+++ Practica5/unix.h
@@ -0,0 +1,80 @@
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define SIZE 4096
+
+typedef struct {
+    sem_t semaphore1;
+    sem_t semaphore2;
+    char buffer[SIZE];
+} shared_data;
+
+void pipeFunction(){
+   const char* sm_name = "shared_memory_string";
+    char* ptr_sm;
+    int shm_fd = shm_open(sm_name, O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd, sizeof(shared_data));
+    shared_data* sm = mmap(0, sizeof(shared_data),  PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    sem_init(&sm->semaphore1, 1, 0);
+    sem_init(&sm->semaphore2, 1, 0);
+
+    pid_t p1;
+    pid_t p2;
+    int fd[2];
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    p1 = fork();
+    if(p1 == 0 ) {
+        
+        char input_str[100];
+        printf("Enter any character string:\n");
+        scanf("%s", input_str);
+        close(fd[0]);
+        write(fd[1], input_str, strlen(input_str) + 1);
+        close(fd[1]);
+        sem_post(&sm->semaphore1);
+        sem_wait(&sm->semaphore2);
+        printf("Modified str: %s\n", sm->buffer);
+        exit(0);
+    }
+   p2 = fork();
+   if (p2 == 0) {
+        sem_wait(&sm->semaphore1);
+        char str_toUpper[100];
+        close(fd[1]);
+        read(fd[0],str_toUpper, sizeof(str_toUpper));
+
+        for(int i=0;str_toUpper[i];i++){
+            str_toUpper[i] =toupper(str_toUpper[i]);
+        }
+        char* write_ptr = sm->buffer;
+        sprintf(write_ptr, "%s", str_toUpper);    
+        close(fd[0]); 
+        sem_post(&sm->semaphore2);
+        exit(0);
+    } 
+
+    close(fd[0]);
+    close(fd[1]);
+    waitpid(p1, NULL, 0);
+    waitpid(p2, NULL, 0);
+    printf("Final string in shared memory: %s\n", sm->buffer);
+    sem_destroy(&sm->semaphore1);
+    sem_destroy(&sm->semaphore2);
+    munmap(sm, sizeof(shared_data));
+    shm_unlink(sm_name);
+    close(shm_fd);
+}
